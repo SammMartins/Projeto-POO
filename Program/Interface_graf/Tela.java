4c53bd4a4bcd6c931b77da483c2bf45c0098f18d
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-01 14:13:34 -0300

    Update Tela.java

--- Program/Interface_graf/Tela.java
+++ Program/Interface_graf/Tela.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 
 public class Tela extends JFrame{
-    public ImageIcon iconInsert = new ImageIcon("Java/Projeto_POO/Images/insert.png");
-    public ImageIcon iconConfig = new ImageIcon("Java/Projeto_POO/Images/config.png");
+    public ImageIcon iconInsert = new ImageIcon("Images/insert.png");
+    public ImageIcon iconConfig = new ImageIcon("Images/config.png");
 
     public Color corFundo1 = new Color(10,25,50);
     public Color corLabel1 = new Color(206,222,250);
