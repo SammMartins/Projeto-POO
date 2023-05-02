f5b623e15572f787b2c3e27609382bfdd88d4940
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-02 19:14:08 -0300

    Update TelaCliPj.java

--- Program/Interface_graf/Start.java
+++ Program/Interface_graf/Start.java
@@ -4,6 +4,7 @@ public class Start  {
 
     public static void main(String[] args) {
         new TelaMain();
+        //new TelaCliPj();
         //new TelaCliPf();
         
     }
--- Program/Interface_graf/TelaCliPJ.java
+++ Program/Interface_graf/TelaCliPJ.java
@@ -1,5 +1,50 @@
 package Program.Interface_graf;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.*;
 
-public class TelaCliPJ extends Tela {
+public class TelaCliPj extends Tela {
     
+    public TelaCliPj(){
+        this.setTitle("Cadastro Pessoa Jurídica");
+        
+        //------------------------------------------------------------------\\
+
+        //Label Razão Social
+        JLabel lRazao = new JLabel();
+        lRazao.setText("Razão Social");
+        lRazao.setBounds(25, 28, 120, 70);
+        lRazao.setForeground(corLabel1);
+        lRazao.setFont(new Font("Arial Black", Font.BOLD, 15));
+        lRazao.setVisible(true);
+        this.add(lRazao);
+
+        //TextArea Razão Social
+        JTextField jtfRazao = new JTextField();
+        jtfRazao.setBounds(170, 50, 300, 25);
+        jtfRazao.setVisible(true);
+        jtfRazao.setFont(new Font("Arial", Font.ITALIC, 12));
+        this.add(jtfRazao);
+
+        //------------------------------------------------------------------
+
+        //Label CNPJ
+        JLabel lCnpj = new JLabel();
+        lCnpj.setText("CNPJ");
+        lCnpj.setBounds(25, 58, 120, 70);
+        lCnpj.setForeground(corLabel1);
+        lCnpj.setFont(new Font("Arial Black", Font.BOLD, 15));
+        lCnpj.setVisible(true);
+        this.add(lCnpj);
+
+        //TextArea CNPJ
+        JTextField jtfCnpj = new JTextField();
+        jtfCnpj.setBounds(170, 80, 300, 25);
+        jtfCnpj.setVisible(true);
+        jtfCnpj.setFont(new Font("Arial", Font.ITALIC, 12));
+        this.add(jtfCnpj);
+
+    }
 }
