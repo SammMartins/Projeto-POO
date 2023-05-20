ee9e4565d2f4c73a2228a1784333c5f77e3cc1d3
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-20 18:14:52 -0300

    Update TelaTable.java

--- Program/Interface_graf/TelaTable.java
+++ Program/Interface_graf/TelaTable.java
@@ -1,6 +1,43 @@
 package Program.Interface_graf;
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TelaTable extends JFrame implements ActionListener {
-    
+    private JTable table;
+    private DefaultTableModel tableModel;
+
+    public TelaTable() {
+        setTitle("Resultado da Busca");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Cria o modelo de dados da tabela
+        tableModel = new DefaultTableModel();
+        tableModel.setColumnIdentifiers(new String[]{"Coluna 1", "Coluna 2", "Coluna 3"});
+
+        // Cria a tabela com o modelo de dados
+        table = new JTable(tableModel);
+
+        // Cria um JScrollPane para permitir a rolagem dos dados
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        // Adiciona o JScrollPane ao JFrame
+        getContentPane().add(scrollPane);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
 }
