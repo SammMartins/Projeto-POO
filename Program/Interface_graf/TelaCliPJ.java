72593546b78e55eedd40eba96a039f335484dffc
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-20 18:49:56 -0300

    Update

--- Program/Interface_graf/TelaCliPJ.java
+++ Program/Interface_graf/TelaCliPJ.java
@@ -8,112 +8,113 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class TelaCliPj extends Tela implements ActionListener {
-    JButton voltar = new JButton();
-    JButton salvar = new JButton("Salvar");
-    public TelaCliPj(){
-        this.setTitle("Cadastro Pessoa Jurídica");
-        
-//------------------------------------------------------------------\\
+        JButton voltar = new JButton();
+        JButton salvar = new JButton("Salvar");
 
-        //Label Razão Social
-        JLabel lRazao = new JLabel();
-        lRazao.setText("Razão Social:");
-        lRazao.setBounds(30, 66, 300, 70);
-        lRazao.setForeground(corLabel1);
-       
-        this.add(lRazao);
+        public TelaCliPj() {
+                this.setTitle("Cadastro Pessoa Jurídica");
 
-        //TextArea Razão Social
-        JTextField jtfRazao = new JTextField();
-        jtfRazao.setBounds(170, 90, 300, 25);
-        jtfRazao.setFont(new Font("Arial", Font.ITALIC, 12));
-        this.add(jtfRazao);
+                // ------------------------------------------------------------------\\
 
-//------------------------------------------------------------------
+                // Label Razão Social
+                JLabel lRazao = new JLabel();
+                lRazao.setText("Razão Social:");
+                lRazao.setBounds(30, 66, 300, 70);
+                lRazao.setForeground(corLabel1);
 
-        //Label CNPJ
-        JLabel lCnpj = new JLabel();
-        lCnpj.setText("CNPJ:");
-        lCnpj.setBounds(30, 100, 120, 70);
-        lCnpj.setForeground(corLabel1);
-        
-        this.add(lCnpj);
+                this.add(lRazao);
 
-        //TextArea CNPJ
-        JTextField jtfCnpj = new JTextField();
-        jtfCnpj.setBounds(170, 124, 300, 25);
-        jtfCnpj.setFont(new Font("Arial", Font.ITALIC, 12));
-        this.add(jtfCnpj);
+                // TextArea Razão Social
+                JTextField jtfRazao = new JTextField();
+                jtfRazao.setBounds(170, 90, 300, 25);
+                jtfRazao.setFont(new Font("Arial", Font.ITALIC, 12));
+                this.add(jtfRazao);
 
-//------------------------------------------------------------------
+                // ------------------------------------------------------------------
 
-        //Label E-mail
-        JLabel lEmail = new JLabel();
-        lEmail.setText("E-mail:");
-        lEmail.setBounds(30, 130, 120, 70);
-        lEmail.setForeground(corLabel1);
-       
-        this.add(lEmail);
+                // Label CNPJ
+                JLabel lCnpj = new JLabel();
+                lCnpj.setText("CNPJ:");
+                lCnpj.setBounds(30, 100, 120, 70);
+                lCnpj.setForeground(corLabel1);
 
-        //TextArea E-mail
-        JTextField jtfEmail = new JTextField();
-        jtfEmail.setBounds(170, 158, 300, 25);
-        jtfEmail.setFont(new Font("Arial", Font.ITALIC, 12));
-        this.add(jtfEmail);
+                this.add(lCnpj);
 
-//------------------------------------------------------------------
+                // TextArea CNPJ
+                JTextField jtfCnpj = new JTextField();
+                jtfCnpj.setBounds(170, 124, 300, 25);
+                jtfCnpj.setFont(new Font("Arial", Font.ITALIC, 12));
+                this.add(jtfCnpj);
 
-        //Label Contato
-        JLabel lContato = new JLabel();
-        lContato.setText("Contato:");
-        lContato.setBounds(30, 168, 120, 70);
-        lContato.setForeground(corLabel1);
-        
-        this.add(lContato);
+                // ------------------------------------------------------------------
 
-        //TextArea Contato
-        JTextField jtfContato = new JTextField();
-        jtfContato.setBounds(170, 192, 300, 25);
-        jtfContato.setFont(new Font("Arial", Font.ITALIC, 12));
-        this.add(jtfContato);
+                // Label E-mail
+                JLabel lEmail = new JLabel();
+                lEmail.setText("E-mail:");
+                lEmail.setBounds(30, 130, 120, 70);
+                lEmail.setForeground(corLabel1);
 
-//------------------------------------------------------------------
+                this.add(lEmail);
 
-        //Label Responsável legal
-        JLabel lResponsavelLegal = new JLabel();
-        lResponsavelLegal.setText("Responsável legal:");
-        lResponsavelLegal.setBounds(30, 202, 300, 70);
-        lResponsavelLegal.setForeground(corLabel1);
-     
-        this.add(lResponsavelLegal);
+                // TextArea E-mail
+                JTextField jtfEmail = new JTextField();
+                jtfEmail.setBounds(170, 158, 300, 25);
+                jtfEmail.setFont(new Font("Arial", Font.ITALIC, 12));
+                this.add(jtfEmail);
 
-        //TextArea Responsável legal
-        JTextField jtfResponsavelLegal = new JTextField();
-        jtfResponsavelLegal.setBounds(170, 226, 300, 25);
-        jtfResponsavelLegal.setFont(new Font("Arial", Font.ITALIC, 12));
-        this.add(jtfResponsavelLegal);
+                // ------------------------------------------------------------------
 
-        //------------------------
-//              Criação de botoes
+                // Label Contato
+                JLabel lContato = new JLabel();
+                lContato.setText("Contato:");
+                lContato.setBounds(30, 168, 120, 70);
+                lContato.setForeground(corLabel1);
 
-        
-        salvar.setVisible(true);
-        salvar.setBounds(175, 285, 120, 40);
-        salvar.setIcon(iconSave);
-        salvar.setHorizontalAlignment(SwingConstants.CENTER);
-        this.add(salvar);
+                this.add(lContato);
 
-        voltar.setIcon(iconBack);
-        voltar.setBounds(10, 15, 40, 30);
-        voltar.addActionListener(this);
-        this.add(voltar);        
-    }
- //----------MÉTODO DE CRIAÇÃO DE EVENTOS----------
-public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == voltar){
-                this.dispose();
-                TelaCli telaCli = new TelaCli();
-                telaCli.telaCli();
+                // TextArea Contato
+                JTextField jtfContato = new JTextField();
+                jtfContato.setBounds(170, 192, 300, 25);
+                jtfContato.setFont(new Font("Arial", Font.ITALIC, 12));
+                this.add(jtfContato);
+
+                // ------------------------------------------------------------------
+
+                // Label Responsável legal
+                JLabel lResponsavelLegal = new JLabel();
+                lResponsavelLegal.setText("Responsável legal:");
+                lResponsavelLegal.setBounds(30, 202, 300, 70);
+                lResponsavelLegal.setForeground(corLabel1);
+
+                this.add(lResponsavelLegal);
+
+                // TextArea Responsável legal
+                JTextField jtfResponsavelLegal = new JTextField();
+                jtfResponsavelLegal.setBounds(170, 226, 300, 25);
+                jtfResponsavelLegal.setFont(new Font("Arial", Font.ITALIC, 12));
+                this.add(jtfResponsavelLegal);
+
+                // ------------------------
+                // Criação de botoes
+
+                salvar.setVisible(true);
+                salvar.setBounds(175, 285, 120, 40);
+                salvar.setIcon(iconSave);
+                salvar.setHorizontalAlignment(SwingConstants.CENTER);
+                this.add(salvar);
+
+                voltar.setIcon(iconBack);
+                voltar.setBounds(10, 15, 40, 30);
+                voltar.addActionListener(this);
+                this.add(voltar);
         }
-}
+
+        // ----------MÉTODO DE CRIAÇÃO DE EVENTOS----------
+        public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == voltar) {
+                        this.dispose();
+                        TelaCli telaCli = new TelaCli();
+                        telaCli.telaCli();
+                }
+        }
 }
--- Program/Interface_graf/TelaTable.java
+++ Program/Interface_graf/TelaTable.java
@@ -1,4 +1,5 @@
 package Program.Interface_graf;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -16,17 +17,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TelaTable extends JFrame implements ActionListener {
+public class TelaTable extends Tela implements ActionListener {
     private JTable table;
     private DefaultTableModel tableModel;
 
     public TelaTable() {
         setTitle("Resultado da Busca");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true); 
+        setSize(500, 400);
+        setLayout(null);
+        getContentPane().setBackground(corFundo1);
 
         // Cria o modelo de dados da tabela
         tableModel = new DefaultTableModel();
-        tableModel.setColumnIdentifiers(new String[]{"Coluna 1", "Coluna 2", "Coluna 3"});
+        tableModel.setColumnIdentifiers(new String[] { "Coluna 1", "Coluna 2", "Coluna 3", "Coluna 4", "Coluna 5" });
 
         // Cria a tabela com o modelo de dados
         table = new JTable(tableModel);
