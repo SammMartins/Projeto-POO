05677b2a723949614af4ff6d096a036cf7157de7
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-24 13:39:17 -0300

    Update Start.java

--- Program/Telas/Start.java
+++ Program/Telas/Start.java
@@ -5,7 +5,7 @@ import Program.ConexãoBD.DAOCliPj;
 public class Start  {
     public static void main(String[] args) {
         new Inicio(); 
-        //new DAOCliPf();     !!!--Com a implementação dessas classes, o programa não roda--!!!
-        //new DAOCliPj();          
+        new DAOCliPf();     
+        new DAOCliPj();          
     }
 }
