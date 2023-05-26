8f7cf53cae31ffc9b61bba39c02e96d1aec15b0c
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-26 00:02:06 -0300

    Update TelaAviso.java

--- Program/Telas/TelaAviso.java
+++ Program/Telas/TelaAviso.java
@@ -5,10 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+
+import Program.ConexãoBD.DAOCliPj;
+
 import javax.swing.*;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
+
 
 public class TelaAviso extends JFrame implements ActionListener {
     public Color corFundo1 = new Color(27, 38, 44);         //definindo a cor de fundo em um objeto
@@ -16,12 +18,16 @@ public class TelaAviso extends JFrame implements ActionListener {
     public Color corLabel1 = new Color(187, 225, 250);     //definindo a cor de fundo em um objeto
     public Color corLabel2 = new Color(200,200,200);    //definindo a cor de fundo em um objeto
 
-    JButton ok = new JButton();
-    
+    JButton cancel = new JButton();
+    JButton confirm = new JButton();
 
-    public ImageIcon iconCheck = new ImageIcon("Images/check.png");
 
-    public TelaAviso(String textoAviso){
+    public ImageIcon iconCancel = new ImageIcon("Images/cancel.png");
+    public ImageIcon iconEnviar = new ImageIcon("Images/enviar-bd.png");
+
+    public int retorno = 0;
+
+    public TelaAviso(){
         setTitle("AVISO");
         setVisible(true);                              //configura visibilidade da tela.
         setSize(250, 200);                 //configurando tamanho da tela.  
@@ -39,42 +45,46 @@ public class TelaAviso extends JFrame implements ActionListener {
 
 
         JLabel labelAviso = new JLabel();
-        labelAviso.setText(formatarTextoAviso(textoAviso));
+        labelAviso.setText("Concluído!");
         labelAviso.setForeground(corLabel1);
-        labelAviso.setBounds(10, 10,100,80);
+        labelAviso.setBounds(10, 10,200,200);
         labelAviso.setFont(new Font("Arial Black", Font.PLAIN, 10));
         this.add(labelAviso);
 
 
-        ok.setBounds(90, 100, 48, 48);
-        ok.setHorizontalAlignment(SwingConstants.CENTER);
-        ok.setIcon(iconCheck);
-        ok.addActionListener(this);
-        this.add(ok);
+        cancel.setBounds(40, 100, 42, 42);
+        cancel.setHorizontalAlignment(SwingConstants.CENTER);
+        cancel.setIcon(iconCancel);
+        cancel.addActionListener(this);
+        cancel.addActionListener(new ActionListener() { //MÉTODO DE CRIAÇÃO DE EVENTO DO BOTÃO SALVAR
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    TelaAviso.this.dispose();
+                    new Inicio(); 
+            }});
+        this.add(cancel);
+
+        confirm.setBounds(140, 100, 42, 42);
+        confirm.setHorizontalAlignment(SwingConstants.CENTER);
+        confirm.setIcon(iconEnviar);
+        confirm.addActionListener(this);
+        confirm.addActionListener(new ActionListener() { //MÉTODO DE CRIAÇÃO DE EVENTO DO BOTÃO SALVAR
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new DAOCliPj();
+                TelaAviso.this.dispose();
+            }});
+        this.add(confirm);
+
+        
     }
 
-    //Método para Formatar o texto de aviso
-    private String formatarTextoAviso(String textoAviso) {
-        StringBuilder textoFormatado = new StringBuilder("<html>");
-        String textoFim;
-
-        int comprimentoMaximoLinha = 100;
-        int comprimentoAtualLinha = 0;
-
-        for (char c : textoAviso.toCharArray()) {
-            textoFormatado.append(c);
-            comprimentoAtualLinha++;
-
-            if (comprimentoAtualLinha >= comprimentoMaximoLinha && c != ' ') {
-                textoFormatado.append("<br>");
-                comprimentoAtualLinha = 0;
-            }
-        }
-
-        textoFormatado.append("</html>");
-        textoFim = textoFormatado.toString();
-        return textoFim;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 
+    
     
 }
