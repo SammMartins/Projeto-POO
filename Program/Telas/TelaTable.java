983b8f98a3ec0cf184d4121fef578699db42c116
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-27 17:25:51 -0300

    Update TelaTable.java

--- Program/Telas/TelaTable.java
+++ Program/Telas/TelaTable.java
@@ -1,55 +1,78 @@
 package Program.Telas;
 
+import Program.Classes.ClientePJ;
+import Program.ConexãoBD.DAOCliPj;
+
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.SwingUtilities;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 
 public class TelaTable extends Tela implements ActionListener{
-    private JTable table;
-    private DefaultTableModel tableModel;
+    DefaultTableModel model = new DefaultTableModel();
+    JTable table = new JTable(model);
+
     JButton voltar = new JButton();
 
     public TelaTable() {
+        // Configurações do JFrame
         setTitle("Resultado da Busca");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
         getContentPane().setBackground(corFundo1);
-        
+        // Configurações do Botão
         voltar.setIcon(iconBack);
         voltar.setBounds(10, 15, 40, 30);
         voltar.addActionListener(this);
         this.add(voltar);
 
-        // Cria a tabela com o modelo de dados
-        table = new JTable(tableModel);
-        this.add(table);
 
-        // Cria o modelo de dados da tabela
-        tableModel = new DefaultTableModel();
-        tableModel.setColumnIdentifiers(new String[] { "Coluna 1", "Coluna 2", "Coluna 3", "Coluna 4", "Coluna 5" });
 
-        // Cria um JScrollPane para permitir a rolagem dos dados
+        // Criação do DefaultTableModel
+        model = new DefaultTableModel();
+        
+        // Criação do JTable
+        table = new JTable(model);
+
+        // Definição das colunas da tabela
+        model.addColumn("Razão Social");
+        model.addColumn("CNPJ");
+        model.addColumn("Email");
+        model.addColumn("Contato");
+        model.addColumn("Responsável");
+
+        // Outras configurações e componentes do JFrame
+
+        // Adicionar o JScrollPane com a tabela ao JFrame
         JScrollPane scrollPane = new JScrollPane(table);
-
-        // Adiciona o JScrollPane ao JFrame
         getContentPane().add(scrollPane);
 
         
         setLocationRelativeTo(null);
     }
+
+    public void preencherTabela() {
+        DAOCliPj dao = new DAOCliPj();
+        List<ClientePJ> clientes = dao.lista();
+    
+        for (ClientePJ cliente : clientes) {
+            Object[] rowData = new Object[5];
+            rowData[0] = cliente.getRazao();
+            rowData[1] = cliente.getCnpj();
+            rowData[2] = cliente.getEmail();
+            rowData[3] = cliente.getContato();
+            rowData[4] = cliente.getResponsavel();
+    
+            model.addRow(rowData);
+        }
+    }
+
+
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == voltar) {
             this.dispose();
