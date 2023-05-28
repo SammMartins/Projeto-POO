83e997445251a0fa1686cb027d965bc08b3297fa
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-28 14:23:57 -0300

    Update TelaTable.java

--- Program/Telas/TelaTable.java
+++ Program/Telas/TelaTable.java
@@ -12,6 +12,10 @@ import java.util.List;
 public class TelaTable extends Tela implements ActionListener {
 
     JButton voltar = new JButton();
+    JButton excluir = new JButton("Excluir");
+    JTable table;
+
+    public ImageIcon iconCancel = new ImageIcon("Images/cancel.png");
 
     public TelaTable() {
         // Configurações do JFrame
@@ -20,12 +24,19 @@ public class TelaTable extends Tela implements ActionListener {
         setLocationRelativeTo(null);
         setLayout(null);
         getContentPane().setBackground(corFundo1);
+        setSize(700, 400);
 
-        // Configurações do Botão
+        // Configurações do Botão voltar
         voltar.setIcon(iconBack);
         voltar.setBounds(10, 15, 40, 30);
         voltar.addActionListener(this);
         this.add(voltar);
+
+        // Configurações do Botão excluir
+        excluir.setBounds(70, 15, 40, 30);
+        excluir.addActionListener(this);
+        excluir.setIcon(iconCancel);
+        add(excluir);
 
         // Criação do DefaultTableModel com as colunas desejadas
         DefaultTableModel model = new DefaultTableModel();
@@ -36,11 +47,11 @@ public class TelaTable extends Tela implements ActionListener {
         model.addColumn("Responsável");
 
         // Criação do JTable com o DefaultTableModel
-        JTable table = new JTable(model);
+        table = new JTable(model);
 
         // Criação do JScrollPane com o JTable
         JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(25, 65, 420, 400);
+        scrollPane.setBounds(25, 65, 625, 250);
         add(scrollPane);
 
         // Preenchimento do DefaultTableModel com os dados do banco de dados
@@ -61,6 +72,12 @@ public class TelaTable extends Tela implements ActionListener {
         if (e.getSource() == voltar) {
             this.dispose();
             new Manuntencao();
+        } else if (e.getSource() == excluir) {
+            int selectedRow = table.getSelectedRow();
+            if (selectedRow != -1) {
+                DefaultTableModel model = (DefaultTableModel) table.getModel();
+                model.removeRow(selectedRow);
+            }
         }
     }
 }
