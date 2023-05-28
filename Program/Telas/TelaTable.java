66ab44519f97df9946de10449cbc6fdee7bc2c2c
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-28 13:48:14 -0300

    Update TelaTable.java

--- Program/Telas/TelaTable.java
+++ Program/Telas/TelaTable.java
@@ -4,23 +4,16 @@ import Program.Classes.ClientePJ;
 import Program.ConexãoBD.DAOCliPj;
 
 import javax.swing.*;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-import java.util.Vector;
 
 public class TelaTable extends Tela implements ActionListener {
 
     JButton voltar = new JButton();
 
     public TelaTable() {
-        // Criação do JTable
-        JTable table = new JTable();
-
         // Configurações do JFrame
         setTitle("Resultado da Busca");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,31 +21,39 @@ public class TelaTable extends Tela implements ActionListener {
         setLayout(null);
         getContentPane().setBackground(corFundo1);
 
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(45, 70, 400, 400);
-        add(scrollPane);
         // Configurações do Botão
         voltar.setIcon(iconBack);
         voltar.setBounds(10, 15, 40, 30);
         voltar.addActionListener(this);
         this.add(voltar);
 
-        // Criação do DefaultTableModel
-        DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+        // Criação do DefaultTableModel com as colunas desejadas
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Razão Social");
+        model.addColumn("CNPJ");
+        model.addColumn("Email");
+        model.addColumn("Contato");
+        model.addColumn("Responsável");
 
-        // Definição das colunas da tabela
+        // Criação do JTable com o DefaultTableModel
+        JTable table = new JTable(model);
+
+        // Criação do JScrollPane com o JTable
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setBounds(25, 65, 420, 400);
+        add(scrollPane);
+
+        // Preenchimento do DefaultTableModel com os dados do banco de dados
         DAOCliPj daoCPJ = new DAOCliPj();
-        for (ClientePJ pj : daoCPJ.lista()) {
-
-            modelo.addRow(new Object[] {
-                    pj.getRazao(),
-                    pj.getCnpj(),
-                    pj.getEmail(),
-                    pj.getContato(),
-                    pj.getResponsavel()
-            });
-            System.out.println(pj.getCnpj()+pj.getRazao());
-
+        List<ClientePJ> clientes = daoCPJ.lista();
+        for (ClientePJ cliente : clientes) {
+            Object[] rowData = new Object[5];
+            rowData[0] = cliente.getRazao();
+            rowData[1] = cliente.getCnpj();
+            rowData[2] = cliente.getEmail();
+            rowData[3] = cliente.getContato();
+            rowData[4] = cliente.getResponsavel();
+            model.addRow(rowData);
         }
     }
 
