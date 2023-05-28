a62d6ff0b2bab558302c6df5bd2281927a0f2e59
José Carlos Neto <zecarlosnetodantas@gmail.com>
2023-05-28 16:10:57 -0300

    Formatação da Tela de avisos

--- Program/Telas/Start.java
+++ Program/Telas/Start.java
@@ -1,10 +1,8 @@
 package Program.Telas;
 
-import com.mysql.cj.xdevapi.Client;
 
 public class Start  {
     public static void main(String[] args) {
-        //new Inicio();   
-        new TelaAvisoPJ(null);    
+        new Inicio();    
     }
 }
