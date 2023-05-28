18962ff1e9b19e0ccd59042c4f465805ec714d1c
José Carlos Neto <zecarlosnetodantas@gmail.com>
2023-05-28 16:09:06 -0300

    Tela de aviso PJ

--- Program/Telas/Start.java
+++ Program/Telas/Start.java
@@ -1,7 +1,10 @@
 package Program.Telas;
 
+import com.mysql.cj.xdevapi.Client;
+
 public class Start  {
     public static void main(String[] args) {
-        new Inicio();           
+        //new Inicio();   
+        new TelaAvisoPJ(null);    
     }
 }
