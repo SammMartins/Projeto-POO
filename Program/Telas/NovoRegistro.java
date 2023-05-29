c3fd75c6cce1bc993a2074223950684ef19eebe2
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-29 13:13:47 -0300

    Update NovoRegistro.java

--- Program/Telas/NovoRegistro.java
+++ Program/Telas/NovoRegistro.java
@@ -6,17 +6,18 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class NovoRegistro extends Tela implements ActionListener{
+public class NovoRegistro extends Tela implements ActionListener {
     JButton PF = new JButton();
     JButton PJ = new JButton();
     JButton voltar = new JButton();
 
-    public NovoRegistro(){
+    public NovoRegistro() {
         setTitle("Gerenciar Clientes");
         setIconImage(icone.getImage());
         setLocationRelativeTo(null);
         setResizable(false);
 
+        // Texto dos desenvolvedores
         JLabel devs = new JLabel("Desenvolvido por: Bruno Cardoso Monteiro, Caio Chaves Lima, José Carlos Neto Soares Dantas, ");
         devs.setForeground(corLabel2);
         devs.setFont(new Font("Arial", Font.ITALIC, 9 ));
@@ -31,64 +32,66 @@ public class NovoRegistro extends Tela implements ActionListener{
         devs2.setVisible(true);
         this.add(devs2);
 
+        // Painel de controle 2
         JPanel panel2 = new JPanel();
         panel2.setBackground(new Color(31, 42, 51));
         panel2.setVisible(true);
         panel2.setBounds(0, 320, 1000, 40);
         this.add(panel2);
 
+        // Título e subtitulo do Pessoa Física
         JLabel labelPF = new JLabel();
         labelPF.setText("Pessoa Física");
         labelPF.setForeground(corLabel1);
-        labelPF.setBounds(180, 60,100 ,100);
+        labelPF.setBounds(180, 60, 100, 100);
         labelPF.setFont(new Font("Arial Black", Font.PLAIN, 12));
         this.add(labelPF);
 
+        // Título e subtitulo do Pessoa Jurídica
         JLabel labelPJ = new JLabel();
         labelPJ.setText("Pessoa Jurídica");
         labelPJ.setForeground(corLabel1);
-        labelPJ.setBounds(180, 170,250,100);
+        labelPJ.setBounds(180, 170, 250, 100);
         labelPJ.setFont(new Font("Arial Black", Font.PLAIN, 12));
         this.add(labelPJ);
-       
-//-------------------------------------------------------------------------------    
-        
+
+        // Botão Pessoa Física
         PF.setBounds(70, 70, 75, 75);
         PF.setIcon(iconPF);
         PF.addActionListener(this);
         this.add(PF);
 
-        
+        // Botão Pessoa Jurídica
         PJ.setIcon(iconPJ);
         PJ.setBounds(70, 190, 75, 75);
         PJ.addActionListener(this);
         this.add(PJ);
 
+        // Botão Voltar
         voltar.setIcon(iconBack);
         voltar.setBounds(10, 15, 40, 30);
         voltar.addActionListener(this);
         this.add(voltar);
-      
     }
-    public void aberturaCli(){
-        this.setVisible(true);        
+
+    public void aberturaCli() {
+        this.setVisible(true);
     }
-    
-  //----------MÉTODO DE CRIAÇÃO DE EVENTOS----------   
+
+    // Método de ação dos botões
     public void actionPerformed(ActionEvent e) {
-       if(e.getSource() == PF){
+        if (e.getSource() == PF) {
             this.dispose();
             new NovoRegistroPF();
-       }
-       if(e.getSource() == PJ){
+        }
+        if (e.getSource() == PJ) {
             this.dispose();
             new NovoRegistroPJ();
-       }
-       if(e.getSource() == voltar){
+        }
+        if (e.getSource() == voltar) {
             this.dispose();
             new Inicio();
-                   
-       }
-        
+        }
+
     }
 }
