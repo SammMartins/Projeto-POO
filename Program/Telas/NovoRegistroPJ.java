632db7f19375157174e0d7b4ca1d61b9eaf2f6b3
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-29 13:15:48 -0300

    Update NovoRegistroPJ.java

--- Program/Telas/NovoRegistroPJ.java
+++ Program/Telas/NovoRegistroPJ.java
@@ -27,30 +27,31 @@ public class NovoRegistroPJ extends Tela implements ActionListener {
                 this.setTitle("Cadastro Pessoa Jurídica");
                 setIconImage(icone.getImage());
                 setResizable(false);
-                
+
+                // Label de desenvolvedores
                 JLabel devs = new JLabel("Desenvolvido por: Bruno Cardoso Monteiro, Caio Chaves Lima, José Carlos Neto Soares Dantas, ");
                 devs.setForeground(corLabel2);
                 devs.setFont(new Font("Arial", Font.ITALIC, 9 ));
                 devs.setBounds(40, 286, 1000, 100);
                 devs.setVisible(true);
                 this.add(devs);
-        
+
+                // Label de desenvolvedores (continuação)
                 JLabel devs2 = new JLabel("Rafael Rodrigues Souza, Sammuel Gusmão Martins - UniFTC");
                 devs2.setForeground(corLabel2);
                 devs2.setFont(new Font("Arial", Font.ITALIC, 9 ));
                 devs2.setBounds(120, 295, 1000, 100);
                 devs2.setVisible(true);
                 this.add(devs2);
-        
+
+                // Painel
                 JPanel panel2 = new JPanel();
                 panel2.setBackground(new Color(31, 42, 51));
                 panel2.setVisible(true);
                 panel2.setBounds(0, 320, 1000, 40);
                 this.add(panel2);
 
-                // ------------------------------------------------------------------\\
-
-                // Label com o nome "RAZÃO SOCIAL"
+                // Label "RAZÃO SOCIAL"
                 JLabel lRazao = new JLabel();
                 lRazao.setText("Razão Social:");
                 lRazao.setBounds(30, 66, 300, 70);
@@ -64,9 +65,7 @@ public class NovoRegistroPJ extends Tela implements ActionListener {
                 jtfRazao.addActionListener(this);
                 this.add(jtfRazao);
 
-                // ------------------------------------------------------------------
-
-                // Label com o texto: "CNPJ"
+                // Label "CNPJ"
                 JLabel lCnpj = new JLabel();
                 lCnpj.setText("CNPJ:");
                 lCnpj.setBounds(30, 100, 120, 70);
@@ -80,9 +79,7 @@ public class NovoRegistroPJ extends Tela implements ActionListener {
                 jtfCnpj.addActionListener(this);
                 this.add(jtfCnpj);
 
-                // ------------------------------------------------------------------
-
-                // Label com o texto: "E-MAIL"
+                // Label "E-MAIL"
                 JLabel lEmail = new JLabel();
                 lEmail.setText("E-mail:");
                 lEmail.setBounds(30, 130, 120, 70);
@@ -96,9 +93,7 @@ public class NovoRegistroPJ extends Tela implements ActionListener {
                 jtfEmail.addActionListener(this);
                 this.add(jtfEmail);
 
-                // ------------------------------------------------------------------
-
-                // Label com o texto: "CONTATO"
+                // Label "CONTATO"
                 JLabel lContato = new JLabel();
                 lContato.setText("Contato:");
                 lContato.setBounds(30, 168, 120, 70);
@@ -112,9 +107,7 @@ public class NovoRegistroPJ extends Tela implements ActionListener {
                 jtfContato.addActionListener(this);
                 this.add(jtfContato);
 
-                // ------------------------------------------------------------------
-
-                // Label com o texto: "RESPONSAVEL LEGAL"
+                // Label "RESPONSAVEL LEGAL"
                 JLabel lResponsavelLegal = new JLabel();
                 lResponsavelLegal.setText("Responsável legal:");
                 lResponsavelLegal.setBounds(30, 202, 300, 70);
@@ -128,58 +121,51 @@ public class NovoRegistroPJ extends Tela implements ActionListener {
                 jtfResponsavelLegal.addActionListener(this);
                 this.add(jtfResponsavelLegal);
 
-                // ------------------------
-
-                // Criação de botoes
-
+                // Botão "Salvar"
                 salvar.setVisible(true);
                 salvar.setBounds(175, 285, 120, 40);
                 salvar.setIcon(iconSave);
                 salvar.setHorizontalAlignment(SwingConstants.CENTER);
                 salvar.addActionListener(new ActionListener() { // MÉTODO DE CRIAÇÃO DE EVENTO DO BOTÃO SALVAR
-                        
-                        public void actionPerformed(ActionEvent e) {
-                                salvar.setEnabled(false); // Desabilita o botão
+                    public void actionPerformed(ActionEvent e) {
+                        salvar.setEnabled(false); // Desabilita o botão
 
-                                sRazao = jtfRazao.getText();
-                                clientePJ.setRazao(sRazao);
+                        sRazao = jtfRazao.getText();
+                        clientePJ.setRazao(sRazao);
 
-                                sCnpj = jtfCnpj.getText();
-                                clientePJ.setCnpj(sCnpj);
+                        sCnpj = jtfCnpj.getText();
+                        clientePJ.setCnpj(sCnpj);
 
-                                sEmail = jtfEmail.getText();
-                                clientePJ.setEmail(sEmail);
+                        sEmail = jtfEmail.getText();
+                        clientePJ.setEmail(sEmail);
 
-                                sContato = jtfContato.getText();
-                                clientePJ.setContato(sContato);
+                        sContato = jtfContato.getText();
+                        clientePJ.setContato(sContato);
 
-                                sResponsavelLegal = jtfResponsavelLegal.getText();
-                                clientePJ.setResponsavel(sResponsavelLegal);
+                        sResponsavelLegal = jtfResponsavelLegal.getText();
+                        clientePJ.setResponsavel(sResponsavelLegal);
 
-                                NovoRegistroPJ.this.dispose();                                
-                                new TelaAvisoPJ(clientePJ);
-                                         
-                        }
+                        NovoRegistroPJ.this.dispose();
+                        new TelaAvisoPJ(clientePJ);
+                    }
                 });
                 this.add(salvar);
 
+                // Botão "Voltar"
                 voltar.setIcon(iconBack);
                 voltar.setBounds(10, 15, 40, 30);
-                // ----------MÉTODO DE CRIAÇÃO DE EVENTOS----------
                 voltar.addActionListener(new ActionListener() {
-                        public void actionPerformed(ActionEvent e) {
-                                NovoRegistroPJ.this.dispose();
-                                new NovoRegistro();
-                        }
+                    public void actionPerformed(ActionEvent e) {
+                        NovoRegistroPJ.this.dispose();
+                        new NovoRegistro();
+                    }
                 });
                 this.add(voltar);
-
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
         }
-
 }
